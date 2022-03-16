302f142d8bc6ff53eda0ea3c1ccca7bbaf9d7701
NIk <ivanovnikita000@gmail.com>
2022-03-16 08:57:31 +0300

    2. Модель данных.

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -2,7 +2,6 @@ package ru.job4j.pojo;
 import java.util.Date;
 
 public class College {
-    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public static void main(String[] args) {
         Student student = new Student();
         student.setName("Ivan");
@@ -11,6 +10,11 @@ public class College {
         student.setStudyGroup("C - 08 - 12");
         student.setReceipt(new Date());
 
-        System.out.println(student.getSurname() + " " + student.getName() + " " + student.getPatronymic() + " study in a group " + student.getStudyGroup() + " since " + student.getReceipt());
+        System.out.println(student.getSurname()
+                + " " + student.getName()
+                + " " + student.getPatronymic()
+                + " study in a group "
+                + student.getStudyGroup() + " since "
+                + student.getReceipt());
     }
 }
