a39fc9909d7da6db84e08f4f33e5f52371b86838
NIk <ivanovnikita000@gmail.com>
2022-06-10 04:09:56 +0300

    1. Singleton

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -4,6 +4,8 @@ public final class SingleTracker {
 
     private static SingleTracker instance;
 
+    private Tracker tracker = new Tracker();
+
     private SingleTracker() {
     }
 
@@ -13,8 +15,6 @@ public final class SingleTracker {
         }
         return instance;
     }
-
-    private Tracker tracker = new Tracker();
 
     public Item add(Item item) {
         return tracker.add(item);
