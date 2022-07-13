e0232d4eab4a03b6e234099be3e0b8d111ab535f
NIk <ivanovnikita000@gmail.com>
2022-07-13 05:28:57 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,9 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] e1 = o1.split("/");
         String[] e2 = o2.split("/");
         int rsl = e2[0].compareTo(e1[0]);
-        if (rsl == 0) {
-            rsl = o1.compareTo(o2);
-        }
-        return rsl;
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
